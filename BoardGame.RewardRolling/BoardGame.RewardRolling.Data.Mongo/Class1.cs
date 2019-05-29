6054cbbfa94c0c48161bb57f9929e3c20ec786a4
hoinx01 <hoinx01@gmail.com>
2019-05-29 22:27:47 +0700

    up

---
+++ BoardGame.RewardRolling/BoardGame.RewardRolling.Data.Mongo/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BoardGame.RewardRolling.Data.Mongo
+{
+    public class Class1
+    {
+    }
+}
---
+++ BoardGame.RewardRolling/BoardGame.RewardRolling.Data.Mongo/Dal/Entities/MdRollingCode.cs
@@ -0,0 +1,14 @@
+﻿using Hinox.Data.Mongo.Dal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGame.RewardRolling.Data.Mongo.Dal.Entities
+{
+    public class MdRollingCode : BaseStringIdMongoEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime? RolledAt { get; set; }
+        public string Status { get; set; }
+    }
+}
