59917c74f1a5bfa9cecfa2fbfee6a20c591d80b7
hoinx01 <hoinx01@gmail.com>
2019-06-12 00:13:15 +0700

    popup create reward

---
+++ BoardGame.RewardRolling/BoardGame.RewardRolling.WebApp/ClientApp/Components/Reward/PopupCreate.vue
@@ -0,0 +1,112 @@
+﻿<template>
+    <b-modal id="popup-create-species"
+             title="Tạo mới quà tặng"
+             size="lg"
+             ref="popupCreateReward"
+             :visible="visible"
+             @hide="complete">
+        <b-form style="width:60%" :novalidate="true">
+            <b-form-group id="fieldset-horizontal"
+                          label-cols-sm="2"
+                          label-cols-lg="2"
+                          description="Tên quà tặng"
+                          label="Tên quà tặng"
+                          label-for="input-name">
+                <b-form-input id="input-name"
+                              v-model="reward.name"
+                              :state="validateName">
+                </b-form-input>
+                <b-form-invalid-feedback :state="validateName">
+                    {{validationMessages.name.invalidLength}}
+                </b-form-invalid-feedback>
+            </b-form-group>
+
+            <b-form-group id="fieldset-horizontal"
+                          label-cols-sm="2"
+                          label-cols-lg="2"
+                          description="Giá trị"
+                          label="Giá trị"
+                          label-for="input-cost">
+                <b-form-input id="input-cost"
+                              v-model="reward.cost"
+                              :state="validateCost">
+                </b-form-input>
+                <b-form-invalid-feedback :state="validateCost">
+                    {{validationMessages.cost.invalidRange}}
+                </b-form-invalid-feedback>
+            </b-form-group>
+        </b-form>
+        <div slot="modal-footer" class="w-100">
+            <b-button variant="primary"
+                      size="sm"
+                      class="float-right"
+                      v-on:click="addReward">
+                OK
+            </b-button>
+        </div>
+    </b-modal>
+</template>
+<script>
+    import cloneDeep from 'lodash/cloneDeep';
+    import rewardRepository from '../../Repositories/RewardRepository';
+
+    export default {
+        name: 'popup-create-reward',
+        props: {
+            visible: {
+                type: Boolean,
+                required: true
+            }
+        },
+        data() {
+            return {
+                reward: {
+                    name: '',
+                    cost:''
+                },
+                validationMessages: {
+                    name: {
+                        invalidLength: 'Tên không được để trống',
+                       
+                    },
+                    cost: {
+                        invalidRange: 'Giá trị quà tặng không được âm'
+                    }
+                }
+            }
+        },
+        computed: {
+            validateName() {
+                return this.reward.name.trim().length >= 1;
+            },
+            validateCost() {
+                return this.reward.cost >= 0;
+            }
+        },
+        watch: {
+            'reward.name': function () {
+                let creatingObject = cloneDeep(this.reward);
+                this.$emit('creating-object-change', creatingObject);
+            },
+            'reward.cost': function () {
+                let creatingObject = cloneDeep(this.reward);
+                this.$emit('creating-object-change', creatingObject);
+            }
+        },
+        methods: {
+            
+            async addReward() {
+                try {
+                    var addedObjects = await speciesRepository.add(this.reward);
+                    this.$emit('success', addedObjects);
+                }
+                catch (error) {
+                    this.$emit('failed', error);
+                }
+            },
+            complete() {
+                this.$emit('completed');
+            }
+        }
+    }
+</script>
--- BoardGame.RewardRolling/BoardGame.RewardRolling.WebApp/ClientApp/Pages/RewardManager.vue
+++ BoardGame.RewardRolling/BoardGame.RewardRolling.WebApp/ClientApp/Pages/RewardManager.vue
@@ -2,6 +2,9 @@
     <div class="router-view-column-flow">
         <div id="main-header" class="router-view-header">
             <div><h1>Danh sách quà tặng</h1></div>
+            <div>
+                <b-btn @click="startCreating">Thêm</b-btn>
+            </div>
         </div>
         <b-list-group id="main-content" class="router-view-content">
             <b-list-group-item class="item-row">
@@ -11,13 +14,16 @@
                 <div class="col-control"></div>
             </b-list-group-item>
             <b-list-group-item class="item-row"
-                               v-for="(item,index) in rewards"
+                               v-for="(item,index) in visibleRewards"
                                :key="item.id">
                 <div class="col-index">{{index + 1}}</div>
                 <div class="col-name">{{item.name}}</div>
                 <div class="col-name">{{item.cost}}</div>
                 <div class="col-control">
-
+                    <font-awesome-icon :icon="['fa', 'trash-alt']"
+                                       @click="startDeleting(item.id)" />
+                    <font-awesome-icon :icon="['fa', 'pencil-alt']"
+                                       @click="startEditing(item.id)" />
                 </div>
             </b-list-group-item>
 
@@ -33,27 +39,75 @@
 
             </pagination>
         </div>
+
+        <div>
+            <popup-create-reward
+                                 @creating-object-change="changeCreating"
+                                 @success="createSuccessfully"
+                                 @completed="createCompletely"
+                                 :visible="currentAction.name=='create'">
+
+            </popup-create-reward>
+        </div>
     </div>
 </template>
 <script>
+    import cloneDeep from 'lodash/cloneDeep';
     import pagination from '../Components/Shared/_Pagination.vue';
+    import PopupCreateReward from '../Components/Reward/PopupCreate.vue';
 
     export default {
         name: 'reward-manager',
         components: {
-            pagination
+            pagination,
+            PopupCreateReward
         },
         data() {
             return {
-
+                currentAction: {
+                    name: null,
+                    status:'',
+                    object: null
+                }
             }
         },
         computed: {
-            rewards() {
-                return this.$store.state.reward.filter.result.rewards;
+            visibleRewards: function () {
+                let elements = cloneDeep(this.$store.state.reward.filter.result.rewards);
+                if (this.currentAction.name == 'create') {
+                    if (this.currentAction.object == null) {
+                        let creating = { name: '', cost: '' };                        
+                        elements.unshift(creating);
+                    }
+                    else
+                        elements.unshift(this.currentAction.object);
+                }
+                elements.forEach((element) => {
+                    element.actionStatus = 'stable';
+                    if (this.currentAction.name == 'edit') {
+                        if (this.currentAction.object.id == element.id) {
+                            element.actionStatus = 'editing';
+                            element.name = this.currentAction.object.name;
+                            element.alias = this.currentAction.object.alias;
+                        }
+                    }
+                });
+                return elements;
             },
             pagination() {
                 return this.$store.state.reward.filter.result.pagination;
+            }
+        },
+        watch: {
+            'currentAction.status': function (newStatus, oldStatus) {
+                console.log(oldStatus + ' -> ' + newStatus)
+                if (newStatus == 'success') {
+                    this.deaction();
+                    this.doFilter();
+                }
+                if (newStatus == 'completed') {
+                    this.deaction();
+                }   
             }
         },
         methods: {
@@ -61,6 +115,33 @@
 
             },
             changePageSize(pageSize) {
+
+            },
+            deaction() {
+                this.currentAction.name = null;
+                this.currentAction.status = '';
+                this.currentAction.object = null;
+            },
+            doFilter() {
+                this.$store.dispatch('reward/DO_FILTER');
+            },
+            startCreating() {
+                this.currentAction.name = 'create';
+                this.currentAction.status = 'continuous';
+            },
+            changeCreating(reward) {
+                this.currentAction.object = reward;
+            },
+            createSuccessfully() {
+                this.currentAction.status = 'success';
+            },
+            createCompletely() {
+
+            },
+            startEditing(rewardId) {
+
+            },
+            startDeleting(rewardId) {
 
             }
         },
