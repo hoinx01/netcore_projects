92511acffb082a5c945dda2ad0f6c9cf3dd4fb70
thuongpd <thuongpd@sapo.vn>
2019-07-24 01:57:56 +0700

    sửa lại validate sữ liệu

--- BoardGame.RewardRolling/BoardGame.RewardRolling.WebApp/Models/RollingCode/CustomerViewModel.cs
+++ BoardGame.RewardRolling/BoardGame.RewardRolling.WebApp/Models/RollingCode/CustomerViewModel.cs
@@ -2,6 +2,7 @@
 using BoardGame.RewardRolling.WebApp.Models.AdministrativeUnit;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,16 @@ namespace BoardGame.RewardRolling.WebApp.Models.Customer
 {
     public class CustomerViewModel
     {
+        [Required(ErrorMessage = "Không được để trống")]
+        [Display(Name = "Tên đăng nhập")]
+        [StringLength(255, ErrorMessage = "{0} tối đa {1} ký tự")]
         public string FullName { get; set; }
+        [Required(ErrorMessage = "Không được để trống")]
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^[0-9\(\)\ \-\+]{10,25}$", ErrorMessage = "Số điện thoại không đúng")]
         public string PhoneNumber { get; set; }
+        [EmailAddress(ErrorMessage = "Email không đúng")]
         public string Email { get; set; }
         public Birthday Birthday { get; set; }
         public int GenderId { get; set; }
